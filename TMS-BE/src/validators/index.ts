d33cb580f88e0be88b863fc3ac9aa3c0def9b277
Saranya-27052006 <saranya27052006@gmail.com>
2026-01-07 06:31:18 +0530

    deleted unwanted folders

--- TMS-BE/src/validators/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Validation Schemas
- * Export all validators for easy importing
- */
-
-export * from './userValidator.js';
-export * from './courseValidator.js';
-export * from './testValidator.js';
-export * from './batchValidator.js';
